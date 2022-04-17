1cd05821f2a7eeea195f4b4da0d95a58a22f19ee
neo <idrisoff1986@mail.ru>
2022-04-17 23:54:52 +0500

    Заполнение базовым набором

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,7 +28,38 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.backgroundWorker2 = new System.ComponentModel.BackgroundWorker();
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Price = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Discount = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Result = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cmsRoot = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.cmsRootAdd = new System.Windows.Forms.ToolStripMenuItem();
+            this.cmsChild = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.cmsChildAdd = new System.Windows.Forms.ToolStripMenuItem();
+            this.cmsModify = new System.Windows.Forms.ToolStripMenuItem();
+            this.cmsDelete = new System.Windows.Forms.ToolStripMenuItem();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.cmsRoot.SuspendLayout();
+            this.cmsChild.SuspendLayout();
             this.SuspendLayout();
             // 
             // tableLayoutPanel1
@@ -36,28 +67,256 @@
             this.tableLayoutPanel1.ColumnCount = 2;
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.treeView1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.groupBox1, 1, 0);
             this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
             this.tableLayoutPanel1.RowCount = 1;
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(818, 461);
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(867, 507);
             this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // treeView1
+            // 
+            this.treeView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.treeView1.Location = new System.Drawing.Point(3, 3);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(427, 501);
+            this.treeView1.TabIndex = 0;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.textBox3);
+            this.groupBox1.Controls.Add(this.button5);
+            this.groupBox1.Controls.Add(this.button4);
+            this.groupBox1.Controls.Add(this.button3);
+            this.groupBox1.Controls.Add(this.dataGridView1);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox1.Location = new System.Drawing.Point(436, 3);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(428, 501);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Расчёты";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Price,
+            this.Discount,
+            this.Result});
+            this.dataGridView1.Location = new System.Drawing.Point(6, 244);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowTemplate.Height = 23;
+            this.dataGridView1.Size = new System.Drawing.Size(413, 248);
+            this.dataGridView1.TabIndex = 5;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Выбор";
+            this.Column1.Name = "Column1";
+            this.Column1.ReadOnly = true;
+            // 
+            // Price
+            // 
+            this.Price.HeaderText = "Цена";
+            this.Price.Name = "Price";
+            this.Price.ReadOnly = true;
+            // 
+            // Discount
+            // 
+            this.Discount.HeaderText = "Скидка";
+            this.Discount.Name = "Discount";
+            this.Discount.ReadOnly = true;
+            // 
+            // Result
+            // 
+            this.Result.HeaderText = "Результат";
+            this.Result.Name = "Result";
+            this.Result.ReadOnly = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(334, 203);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Сохранить";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(105, 203);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(223, 21);
+            this.textBox2.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(334, 145);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Расчитать";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(105, 145);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(223, 21);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(22, 206);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Результат";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 145);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Введите цену";
+            // 
+            // cmsRoot
+            // 
+            this.cmsRoot.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.cmsRootAdd});
+            this.cmsRoot.Name = "cmsRoot";
+            this.cmsRoot.Size = new System.Drawing.Size(94, 26);
+            // 
+            // cmsRootAdd
+            // 
+            this.cmsRootAdd.Name = "cmsRootAdd";
+            this.cmsRootAdd.Size = new System.Drawing.Size(93, 22);
+            this.cmsRootAdd.Text = "Add";
+            // 
+            // cmsChild
+            // 
+            this.cmsChild.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.cmsChildAdd,
+            this.cmsModify,
+            this.cmsDelete});
+            this.cmsChild.Name = "cmsChild";
+            this.cmsChild.Size = new System.Drawing.Size(107, 70);
+            // 
+            // cmsChildAdd
+            // 
+            this.cmsChildAdd.Name = "cmsChildAdd";
+            this.cmsChildAdd.Size = new System.Drawing.Size(106, 22);
+            this.cmsChildAdd.Text = "Add";
+            // 
+            // cmsModify
+            // 
+            this.cmsModify.Name = "cmsModify";
+            this.cmsModify.Size = new System.Drawing.Size(106, 22);
+            this.cmsModify.Text = "Modify";
+            // 
+            // cmsDelete
+            // 
+            this.cmsDelete.Name = "cmsDelete";
+            this.cmsDelete.Size = new System.Drawing.Size(106, 22);
+            this.cmsDelete.Text = "Delete";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(22, 18);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Add";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(22, 57);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "Edit";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(22, 98);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 6;
+            this.button5.Text = "Delete";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(105, 20);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(169, 21);
+            this.textBox3.TabIndex = 7;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(818, 461);
+            this.ClientSize = new System.Drawing.Size(867, 507);
             this.Controls.Add(this.tableLayoutPanel1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.MyFormLoad);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.cmsRoot.ResumeLayout(false);
+            this.cmsChild.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
-
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker2;
         private TableLayoutPanel tableLayoutPanel1;
+        private TreeView treeView1;
+        private GroupBox groupBox1;
+        private Button button2;
+        private TextBox textBox2;
+        private Button button1;
+        private TextBox textBox1;
+        private Label label2;
+        private Label label1;
+        private DataGridView dataGridView1;
+        private DataGridViewTextBoxColumn Column1;
+        private DataGridViewTextBoxColumn Price;
+        private DataGridViewTextBoxColumn Discount;
+        private DataGridViewTextBoxColumn Result;
+        private ContextMenuStrip cmsRoot;
+        private ToolStripMenuItem cmsRootAdd;
+        private ContextMenuStrip cmsChild;
+        private ToolStripMenuItem cmsChildAdd;
+        private ToolStripMenuItem cmsModify;
+        private ToolStripMenuItem cmsDelete;
+        private Button button5;
+        private Button button4;
+        private Button button3;
+        private TextBox textBox3;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -1,10 +1,119 @@
+using Microsoft.Data.Sqlite;
+using System.Data;
+
 namespace SalonLorena
 {
     public partial class Form1 : Form
     {
+        string path = "data.db";
+
         public Form1()
         {
             InitializeComponent();
+            button3.Click += (o, e) => { CreateNode(treeView1, textBox3.Text, "A"); };
+            button4.Click += (o, e) => { CreateNode(treeView1, textBox3.Text, "E"); };
+            button5.Click += (o, e) => { CreateNode(treeView1, textBox3.Text, "D"); };
+        }
+        void CreateDB()
+        {
+            using (var con = new SqliteConnection(@"Data Source=" + path))
+            {
+                con.Open();
+                string sql1 = "create table if not exists shops(" +
+                    "id integer not null primary key autoincrement unique, " +
+                    "name varchar(20) not null unique, " +
+                    "discount real default 0, " +
+                    "description varchar(124) default '', " +
+                    "depend integer default 0, " +
+                    "parentId integer)";
+                SqliteCommand command = new SqliteCommand(sql1, con);
+                command.ExecuteNonQuery();
+                string stm = "SELECT * FROM shops";
+                var cmd = new SqliteCommand(stm, con);
+                var reader = cmd.ExecuteReader();
+                if (!reader.HasRows)
+                {
+                    string sql2 = @"insert into shops(name, discount, description, depend, parentId) values "
+                    + "('Миасс', 4, '', 0, 0), "
+                    + "('Амелия', 5, '', 1, 1), "
+                    + "('Тест1', 2, '', 1, 2), "
+                    + "('Тест2', 0, '', 1, 1), "
+                    + "('Курган', 11, '', 0, 0)";
+                    command = new SqliteCommand(sql2, con);
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+        void MyFormLoad(object sender, EventArgs e)
+        {
+            CreateDB();
+            ShowData();
+        }
+        void CreateNode(TreeView view, string text, string type)
+        {
+            TreeNode node = new TreeNode(text);
+            if (type == "A")
+            {
+                try
+                {
+                    view.SelectedNode.Nodes.Add(node);
+                }
+                catch (Exception)
+                { view.Nodes.Add(node); }
+            }
+            else if (type == "E")
+                view.SelectedNode.Text = text;
+            else
+                view.SelectedNode.Remove();
+        }
+        void ShowData()
+        {
+            treeView1.Nodes.Clear();
+            using (var con = new SqliteConnection(@"Data Source=" + path))
+            {
+                con.Open();
+                string stm = "SELECT * FROM shops where parentId = 0";
+                string stmChild = "SELECT * FROM shops where parentId > 0";
+                var cmd = new SqliteCommand(stm, con);
+                var cmdChild = new SqliteCommand(stmChild, con);
+                var readerChild = cmdChild.ExecuteReader();
+                var reader = cmd.ExecuteReader();
+                treeView1.Nodes.Add("Салоны");
+                while (reader.Read())
+                {
+                    if (reader.HasRows)
+                    {
+                        treeView1.Nodes[0].Nodes.Add(reader.GetString(1)).Tag = reader.GetString(0);
+                    }
+                }
+                while (readerChild.Read())
+                {
+                    if (readerChild.HasRows)
+                    {
+                        var node = new TreeNode(readerChild.GetString(1));
+                        node.Tag = readerChild.GetString(0);
+                        FindByTag(treeView1.Nodes[0].Nodes, node, Convert.ToInt32(readerChild.GetString(5)));
+                    }
+                }
+                treeView1.ExpandAll();
+            }
+        }
+        private void FindByTag(TreeNodeCollection Nodes, TreeNode node, int tagValue)
+        {
+            for (int i = 0; i < Nodes.Count; i++)
+            {
+                //Не забываем, что в этом примере в Tag мы записывали числовой первичный ключ!
+                if (Convert.ToInt32(Nodes[i].Tag) == tagValue)
+                {
+                    //Раскрываем нужный пункт 
+                    Nodes[i].Nodes.Add(node);
+                }
+                else
+                {
+                    //Запускаем рекурсию
+                    FindByTag(Nodes[i].Nodes, node, tagValue);
+                }
+            }
         }
     }
 }
